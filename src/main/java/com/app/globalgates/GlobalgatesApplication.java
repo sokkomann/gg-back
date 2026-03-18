89481fc559e4e8fa9cebc620bbdec0093fc607ea
fullypigchan <pigchan0202@gmail.com>
2026-03-18 17:03:14 +0900

    init'

---
+++ src/main/java/com/app/globalgates/GlobalgatesApplication.java
@@ -0,0 +1,13 @@
+package com.app.globalgates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalgatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalgatesApplication.class, args);
+    }
+
+}
